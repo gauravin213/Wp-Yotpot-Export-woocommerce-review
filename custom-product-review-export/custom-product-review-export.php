72bd0db7b1b8f670879a7afcbbbbe7ee0d9d72cb
gauravin213 <33614898+gauravin213@users.noreply.github.com>
2020-04-13 12:51:04 +0530

    Update custom-product-review-export.php

--- custom-product-review-export/custom-product-review-export.php
+++ custom-product-review-export/custom-product-review-export.php
@@ -63,163 +63,9 @@ function CustomProductReviewExportAction_fun(){
     if ( ! current_user_can( 'manage_options' ) )
         return;
 
-
         global $wpdb;
 
-
-$pppp = array(31693,
-31020,
-38217,
-29761,
-29378,
-31693,
-92286,
-87237,
-87349,
-31695,
-92286,
-32420,
-32459,
-92286,
-28392,
-30202,
-92286,
-36746,
-28589,
-92286,
-29356,
-54114,
-29195,
-77062,
-92286,
-28419,
-31584,
-33085,
-31545,
-31695,
-36729,
-29084,
-28254,
-31551,
-32027,
-28589,
-28694,
-31651,
-32211,
-42487,
-37471,
-33083,
-29024,
-86180,
-30001,
-78950,
-29741,
-87843,
-28661,
-87896,
-29378,
-30313,
-28681,
-42487,
-29631,
-35536,
-29971,
-29084,
-29971,
-30313,
-30108,
-31567,
-33691,
-29759,
-30211,
-28254,
-29717,
-29294,
-29841,
-31020,
-30299,
-30108,
-28398,
-31590,
-82060,
-31006,
-79659,
-78848,
-92073,
-28694,
-29717,
-78514,
-79659,
-78911,
-32064,
-85433,
-32455,
-33086,
-29466,
-31420,
-51948,
-79659,
-78514,
-82825,
-28681,
-29084,
-32459,
-29390,
-28479,
-28936,
-29428,
-30634,
-33089,
-30001,
-93913,
-86120,
-78922,
-82751,
-37541,
-37541,
-28967,
-94126,
-30606,
-35538,
-31261,
-29500,
-28802,
-29536,
-29047,
-28724,
-29002,
-78548,
-58341,
-28694,
-30854,
-36680,
-31545,
-29759,
-42489,
-92692,
-93684,
-29627,
-31590,
-37553,
-29959,
-28562,
-29951,
-78877,
-33691,
-79659,
-30055,
-35542,
-28339,
-35532,
-37471,
-);
-
-
-$pppp_i = implode(', ', $pppp);
-
- 
-
-        echo $query = "SELECT comment_post_ID AS product_id, 
+        $query = "SELECT comment_post_ID AS product_id, 
                          comment_author AS display_name, 
                          comment_date AS date,
                          comment_author_email AS user_email, 
@@ -231,8 +77,7 @@ $pppp_i = implode(', ', $pppp);
                   FROM `".$wpdb->prefix."comments` 
                   INNER JOIN `".$wpdb->prefix."posts` ON `".$wpdb->prefix."posts`.`ID` = `".$wpdb->prefix."comments`.`comment_post_ID` 
                   INNER JOIN `".$wpdb->prefix."commentmeta` ON `".$wpdb->prefix."commentmeta`.`comment_id` = `".$wpdb->prefix."comments`.`comment_ID` 
-                  WHERE `post_type` = 'product' AND meta_key='rating' AND ID IN (".$pppp_i.") AND comment_content = ''";
-
+                  WHERE `post_type` = 'product' AND meta_key='rating'";
 
        
         $results = $wpdb->get_results($query);
@@ -243,7 +88,7 @@ $pppp_i = implode(', ', $pppp);
             $current_review = array();  
             $review_content = custom_cleanContent($value->review_content); //
 
-            $current_review['product_id'] = $value->product_id;
+            /*$current_review['product_id'] = $value->product_id;
             $current_review['product_title'] = $value->product_title;
             $current_review['product_url'] = get_permalink($value->product_id);;
             $current_review['date'] = $value->date;
@@ -261,9 +106,9 @@ $pppp_i = implode(', ', $pppp);
             $current_review['comment_created_at'] = $value->date;
             $current_review['published_image_url'] = "";
             $current_review['unpublished_image_url'] = "";
-            $current_review['cf_Y__X'] = "";
+            $current_review['cf_Y__X'] = "";*/
 
-            /*$current_review['0'] = $value->product_id;
+            $current_review['0'] = $value->product_id;
             $current_review['1'] = $value->product_title;
             $current_review['2'] = get_permalink($value->product_id);;
             $current_review['3'] = $value->date;
@@ -281,12 +126,12 @@ $pppp_i = implode(', ', $pppp);
             $current_review['15'] = $value->date;
             $current_review['16'] = "";
             $current_review['17'] = "";
-            $current_review['18'] = "";*/
+            $current_review['18'] = "";
 
             $all_reviews[] = $current_review;
         }
 
-        echo "<pre>====>"; print_r($all_reviews); echo "</pre>";die(); 
+        //echo "<pre>====>"; print_r($all_reviews); echo "</pre>";die(); 
 
         $header_row = array(
             'product_id',
